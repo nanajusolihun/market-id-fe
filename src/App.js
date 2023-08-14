2196d1b96144c1331924cf3ee5a048fb38635aef
Nana Jusolihun <nanajusolihun@gmail.com>
2023-08-14 09:45:34 +0700

    feature : create style app.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Outlet } from "react-router-dom";
+import "./assets/css/app.css"
 
 const App = () => {
-  return <Outlet />;
+  return (
+    <main className="app__main">
+      <Outlet />;
+    </main>
+  ) 
 };
 
 export default App;
