5fb3ef30c2c5e0482c11b1fc0a9402a96e4ca444
Nana Jusolihun <nanajusolihun@gmail.com>
2023-08-24 15:44:52 +0700

    feature: handle router guard role admin at layoutAuth

--- src/layouts/LayoutAuth.jsx
+++ src/layouts/LayoutAuth.jsx
@@ -1,10 +1,19 @@
 import { Navigate, useLocation } from "react-router-dom";
 
 const LayoutAuth = ({ auth, children }) => {
-  const { token } = auth;
+  const { token, user } = auth;
   const location = useLocation();
 
   const isPage = ["/login", "/register"].includes(location.pathname);
+
+  // ROLE ADMIN
+  if (user.role && ["admin"].includes(user.role.name)) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    if (isPage) return children;
+    return <Navigate to="/login" replace />;
+  }
 
   if (!token) {
     if (isPage) return children;
