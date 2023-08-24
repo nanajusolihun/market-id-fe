5884665e13780113da0d5b55a64fd95b03b4ae4c
Nana Jusolihun <nanajusolihun@gmail.com>
2023-08-24 15:09:58 +0700

    feature: add isCheckout, isStatus

--- src/components/Carts/CartsChecouts.jsx
+++ src/components/Carts/CartsChecouts.jsx
@@ -10,19 +10,22 @@ import handleErrorMessage from "../../utils/handleErrorMessage";
 import FormatCurrency from "../../utils/currency";
 import PopupCheckout from "./PopupCheckout";
 
-export default function CartsCheckouts() {
+export default function CartsCheckouts(props) {
+  const { isCheckouts = false, isStatus = true, detailInvoices = { address_id: "", sub_total: 0, ppn: 0, total: 0 }, handleConfirmDone = () => {} } = props;
   const [optionsAddress, setOptionsAddress] = useState([]);
+  const [fullAddress, setFullAddress] = useState("-");
 
   const { dataCart } = useSelector((state) => state.carts);
   const dataSubTotal = dataCart.map((cart) => cart.sub_total);
-  const subTotal = dataSubTotal.reduce((a, b) => a + b);
+  const subTotal = dataSubTotal.reduce((a, b) => a + b, 0);
   const PPN10percen = subTotal * 0.1;
   const total = subTotal + PPN10percen;
 
   const dispatch = useDispatch();
 
+  // SELECT ADDRESS FOR CHECKOUT
   useEffect(() => {
-    if (!optionsAddress.length) {
+    if (!optionsAddress.length && isCheckouts) {
       dispatch({ type: "SET_LOADING", value: true });
       axios
         .get("/address/list")
@@ -41,9 +44,32 @@ export default function CartsCheckouts() {
           dispatch({ type: "SET_LOADING", value: false });
         });
     }
-  }, [optionsAddress, dispatch]);
+  }, [optionsAddress, dispatch, isCheckouts]);
 
-  const [fullAddress, setFullAddress] = useState("-");
+  // FOR DETAIL ADDRESS INVOICE
+  useEffect(() => {
+    if (detailInvoices.address_id.length > 0) {
+      dispatch({ type: "SET_LOADING", value: true });
+      axios
+        .get(`/address/${detailInvoices.address_id}/detail`)
+        .then((response) => {
+          const { name, address, village, district, regency, province, passcode } = response.data.data;
+          setFullAddress(`${name} : ${address}, ${village.name}, ${district.name}, ${regency.name}, ${province.name}, ${passcode}`);
+        })
+        .catch((error) => {
+          // TOAST ERROR
+          const message = error.response?.data?.message;
+          toast(handleErrorMessage(message), {
+            position: toast.POSITION.TOP_RIGHT,
+            type: toast.TYPE.ERROR,
+          });
+        })
+        .finally(() => {
+          dispatch({ type: "SET_LOADING", value: false });
+        });
+    }
+  }, [detailInvoices]);
+
   const [address, setAddress] = useState({
     _id: "",
     name: "",
@@ -117,49 +143,83 @@ export default function CartsCheckouts() {
 
   return (
     <>
-      <Card>
+      <Card className="w-100">
         <Card.Body>
-          <Form.Label htmlFor="form-address" className="fw-bold">
-            Address
-          </Form.Label>
-          <Form.Select id="form-address" value={address._id} onChange={handleChangeAddress}>
-            <option value="">Select your address</option>
-            {optionsAddress.map((address, index) => (
-              <option key={`address-${index}`} value={address._id}>
-                {address.name}
-              </option>
-            ))}
-          </Form.Select>
-          <Card.Text className="mt-2 mb-4">{fullAddress}</Card.Text>
+          {isCheckouts ? (
+            <>
+              <Form.Label htmlFor="form-address" className="fw-bold mt-2">
+                ADDRESS
+              </Form.Label>
+              <Form.Select id="form-address" value={address._id} onChange={handleChangeAddress}>
+                <option value="">Select your address</option>
+                {optionsAddress.map((address, index) => (
+                  <option key={`address-${index}`} value={address._id}>
+                    {address.name}
+                  </option>
+                ))}
+              </Form.Select>
+              <Card.Text className="mt-2 mb-5">{fullAddress}</Card.Text>
 
-          <div className="  d-flex justify-content-between align-items-center mb-3">
-            <Card.Subtitle>Sub Total</Card.Subtitle>
-            <Card.Subtitle>{FormatCurrency(subTotal)}</Card.Subtitle>
-          </div>
+              <div className="  d-flex justify-content-between align-items-center my-3">
+                <Card.Subtitle>Sub Total</Card.Subtitle>
+                <Card.Subtitle>{FormatCurrency(subTotal)}</Card.Subtitle>
+              </div>
 
-          <div className=" d-flex justify-content-between align-items-center mb-3">
-            <Card.Subtitle>PPN (10%)</Card.Subtitle>
-            <Card.Subtitle>{FormatCurrency(PPN10percen)}</Card.Subtitle>
-          </div>
+              <div className=" d-flex justify-content-between align-items-center my-3">
+                <Card.Subtitle>PPN (10%)</Card.Subtitle>
+                <Card.Subtitle>{FormatCurrency(PPN10percen)}</Card.Subtitle>
+              </div>
 
-          <div className=" d-flex justify-content-between align-items-center mb-4">
-            <Card.Subtitle>Total</Card.Subtitle>
-            <Card.Subtitle>{FormatCurrency(total)}</Card.Subtitle>
-          </div>
+              <div className=" d-flex justify-content-between align-items-center my-4">
+                <Card.Subtitle className="fs-5 fw-bold">Total Price</Card.Subtitle>
+                <Card.Subtitle className=" fs-5 fw-bold">{FormatCurrency(total)}</Card.Subtitle>
+              </div>
 
-          <Button
-            disabled={address._id.length === 0}
-            variant={address._id ? "primary" : "secondary"}
-            className="w-100 fw-bold"
-            onClick={() => {
-              setShow(true);
-            }}
-          >
-            Checkout
-          </Button>
+              <Button
+                size="lg"
+                disabled={address._id.length === 0}
+                variant={address._id ? "primary" : "secondary"}
+                className="mt-4 w-100 fw-bold"
+                onClick={() => {
+                  setShow(true);
+                }}
+              >
+                CHECKOUT PRODUCT
+              </Button>
+            </>
+          ) : (
+            <>
+              <Form.Label htmlFor="form-address" className="fw-bold mt-2">
+                ADDRESS :
+              </Form.Label>
+              <Card.Text className="mt-2 mb-5">{fullAddress}</Card.Text>
+
+              <div className="  d-flex justify-content-between align-items-center my-3">
+                <Card.Subtitle>Sub Total</Card.Subtitle>
+                <Card.Subtitle>{FormatCurrency(detailInvoices.sub_total)}</Card.Subtitle>
+              </div>
+
+              <div className=" d-flex justify-content-between align-items-center my-3">
+                <Card.Subtitle>PPN (10%)</Card.Subtitle>
+                <Card.Subtitle>{FormatCurrency(detailInvoices.ppn)}</Card.Subtitle>
+              </div>
+
+              <div className=" d-flex justify-content-between align-items-center my-4">
+                <Card.Subtitle className="fs-5 fw-bold">Total Price</Card.Subtitle>
+                <Card.Subtitle className=" fs-5 fw-bold">{FormatCurrency(detailInvoices.total)}</Card.Subtitle>
+              </div>
+
+              {!isStatus && (
+                <Button size="lg" variant="success" className="mt-4 w-100 fw-bold" onClick={handleConfirmDone}>
+                  CONFIRM DONE
+                </Button>
+              )}
+            </>
+          )}
         </Card.Body>
       </Card>
 
+      {/* POPUP CHECKOUT */}
       <PopupCheckout
         show={show}
         handleCancel={() => {
